6a940de1de9ce236c43cd9ade1ed896091a4a2c3
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-27 01:59:53 -0500

    Commit #40

--- saas-backend/src/auth/auth.module.ts
+++ saas-backend/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 /* saas-backend/src/auth/auth.module.ts */
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
-    controllers: [AuthController]
+    controllers: [AuthController],
+    providers: [AuthService]
 })
 export class AuthModule { }
