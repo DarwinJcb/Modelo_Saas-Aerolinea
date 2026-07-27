9760d0fb3bebf9745d78f5fe8a5333fbfdd47d27
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-26 23:42:31 -0500

    Commit #20

--- saas-backend/src/main.ts
+++ saas-backend/src/main.ts
@@ -1,9 +1,26 @@
 /* saas-backend/src/main.ts */
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
