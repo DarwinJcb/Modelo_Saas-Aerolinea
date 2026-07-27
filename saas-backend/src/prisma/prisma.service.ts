c99c7d399539e3c5cab7f1ce4a8e00db802fee58
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-26 23:35:24 -0500

    Commit #16

--- saas-backend/src/prisma/prisma.service.ts
+++ saas-backend/src/prisma/prisma.service.ts
@@ -1,5 +1,35 @@
 /* saas-backend/src/prisma/prisma.service.ts */
-import { Injectable } from '@nestjs/common';
+import 'dotenv/config';
+import { Injectable, OnModuleDestroy, OnModuleInit, } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
 
 @Injectable()
-export class PrismaService { }
+export class PrismaService
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        const cadenaConexion = process.env.DATABASE_URL;
+        if (!cadenaConexion) {
+            throw new Error(
+                'La variable DATABASE_URL no está definida en el archivo .env',
+            );
+        }
+
+        const adaptadorPostgresql = new PrismaPg({
+            connectionString: cadenaConexion,
+        });
+
+        super({
+            adapter: adaptadorPostgresql,
+        });
+    }
+
+    async onModuleInit(): Promise<void> {
+        await this.$connect();
+    }
+
+    async onModuleDestroy(): Promise<void> {
+        await this.$disconnect();
+    }
+}
