96384bae503542025190a6853898be13134049d1
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-26 23:31:59 -0500

    Commit #14

--- saas-backend/src/prisma/prisma.module.ts
+++ saas-backend/src/prisma/prisma.module.ts
@@ -1,5 +1,8 @@
 /* saas-backend/src/prisma/prisma.module.ts */
 import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
-@Module({})
+@Module({
+  providers: [PrismaService]
+})
 export class PrismaModule { }
