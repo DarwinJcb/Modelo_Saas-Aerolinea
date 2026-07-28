5a75d24247501fa40d1dc31e508589bb9bfe1805
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-28 16:43:16 -0500

    Commit #90

--- saas-backend/src/suscripciones/suscripciones.controller.ts
+++ saas-backend/src/suscripciones/suscripciones.controller.ts
@@ -1,25 +1,29 @@
 /* saas-backend/src/suscripciones/suscripciones.controller.ts */
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards, } from '@nestjs/common';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { RolUsuario } from '../generated/prisma/enums';
 import { CreateSuscripcionDto } from './dto/create-suscripcion.dto';
 import { UpdateSuscripcionDto } from './dto/update-suscripcion.dto';
 import { SuscripcionesService } from './suscripciones.service';
 
+@Roles(RolUsuario.SUPERADMIN)
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('suscripciones')
 export class SuscripcionesController {
-  constructor(private readonly suscripcionesService: SuscripcionesService) {}
+  constructor(
+    private readonly suscripcionesService: SuscripcionesService,
+  ) { }
 
   @Post()
-  create(@Body() createSuscripcionDto: CreateSuscripcionDto) {
-    return this.suscripcionesService.create(createSuscripcionDto);
+  create(
+    @Body()
+    createSuscripcionDto: CreateSuscripcionDto,
+  ) {
+    return this.suscripcionesService.create(
+      createSuscripcionDto,
+    );
   }
 
   @Get()
@@ -32,14 +36,17 @@ export class SuscripcionesController {
     @Param('idSuscripcion', ParseIntPipe)
     idSuscripcion: number,
   ) {
-    return this.suscripcionesService.findOne(idSuscripcion);
+    return this.suscripcionesService.findOne(
+      idSuscripcion,
+    );
   }
 
   @Patch(':idSuscripcion')
   update(
     @Param('idSuscripcion', ParseIntPipe)
     idSuscripcion: number,
-    @Body() updateSuscripcionDto: UpdateSuscripcionDto,
+    @Body()
+    updateSuscripcionDto: UpdateSuscripcionDto,
   ) {
     return this.suscripcionesService.update(
       idSuscripcion,
@@ -52,6 +59,8 @@ export class SuscripcionesController {
     @Param('idSuscripcion', ParseIntPipe)
     idSuscripcion: number,
   ) {
-    return this.suscripcionesService.remove(idSuscripcion);
+    return this.suscripcionesService.remove(
+      idSuscripcion,
+    );
   }
 }
