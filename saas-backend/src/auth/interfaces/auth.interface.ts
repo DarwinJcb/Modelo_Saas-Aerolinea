55e1a240a6037bdb98e1ab93ed836f47fa686b19
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-28 15:12:54 -0500

    Commit #75

--- saas-backend/src/auth/interfaces/auth.interface.ts
+++ saas-backend/src/auth/interfaces/auth.interface.ts
@@ -1,2 +1,32 @@
 /* saas-backend/src/auth/interfaces/auth.interface.ts */
-export interface Auth { }
+import { EstadoAerolinea, EstadoUsuario, RolUsuario, } from '../../generated/prisma/enums';
+
+export interface JwtPayload {
+    sub: number;
+    rolUsuario: RolUsuario;
+    fkAerolineaUsuario: number | null;
+    iat?: number;
+    exp?: number;
+}
+
+export interface UsuarioAutenticado {
+    idUsuario: number;
+    fkAerolineaUsuario: number | null;
+    nombresUsuario: string;
+    apellidosUsuario: string;
+    correoUsuario: string;
+    rolUsuario: RolUsuario;
+    estadoUsuario: EstadoUsuario;
+    aerolineaUsuario: {
+        idAerolinea: number;
+        nombreComercialAerolinea: string;
+        estadoAerolinea: EstadoAerolinea;
+    } | null;
+}
+
+export interface SolicitudConUsuario {
+    headers: {
+        authorization?: string;
+    };
+    usuario?: UsuarioAutenticado;
+}
