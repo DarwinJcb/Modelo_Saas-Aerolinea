962f8c52755680d10f1ec5585f8ae6367f674bd1
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-28 15:56:04 -0500

    Commit #84

--- saas-backend/src/auth/auth.controller.ts
+++ saas-backend/src/auth/auth.controller.ts
@@ -4,6 +4,9 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import type { SolicitudConUsuario } from './interfaces/auth.interface';
+import { RolUsuario } from '../generated/prisma/enums';
+import { Roles } from './decorators/roles.decorator';
+import { RolesGuard } from './guards/roles.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -17,7 +20,8 @@ export class AuthController {
         return this.authService.login(loginDto);
     }
 
-    @UseGuards(JwtAuthGuard)
+    @Roles(RolUsuario.SUPERADMIN)
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Get('perfil')
     perfil(@Req() solicitud: SolicitudConUsuario) {
         return solicitud.usuario;
