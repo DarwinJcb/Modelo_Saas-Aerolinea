b57503731a23647872553a1fba30d650cf273de5
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-28 16:34:06 -0500

    Commit #89

--- saas-backend/src/aerolineas/aerolineas.controller.ts
+++ saas-backend/src/aerolineas/aerolineas.controller.ts
@@ -1,25 +1,29 @@
 /* saas-backend/src/aerolineas/aerolineas.controller.ts */
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards, } from '@nestjs/common';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { RolUsuario } from '../generated/prisma/enums';
 import { AerolineasService } from './aerolineas.service';
 import { CreateAerolineaDto } from './dto/create-aerolinea.dto';
 import { UpdateAerolineaDto } from './dto/update-aerolinea.dto';
 
+@Roles(RolUsuario.SUPERADMIN)
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('aerolineas')
 export class AerolineasController {
-  constructor(private readonly aerolineasService: AerolineasService) {}
+  constructor(
+    private readonly aerolineasService: AerolineasService,
+  ) { }
 
   @Post()
-  create(@Body() createAerolineaDto: CreateAerolineaDto) {
-    return this.aerolineasService.create(createAerolineaDto);
+  create(
+    @Body()
+    createAerolineaDto: CreateAerolineaDto,
+  ) {
+    return this.aerolineasService.create(
+      createAerolineaDto,
+    );
   }
 
   @Get()
@@ -28,20 +32,35 @@ export class AerolineasController {
   }
 
   @Get(':idAerolinea')
-  findOne(@Param('idAerolinea', ParseIntPipe) idAerolinea: number) {
-    return this.aerolineasService.findOne(idAerolinea);
+  findOne(
+    @Param('idAerolinea', ParseIntPipe)
+    idAerolinea: number,
+  ) {
+    return this.aerolineasService.findOne(
+      idAerolinea,
+    );
   }
 
   @Patch(':idAerolinea')
   update(
-    @Param('idAerolinea', ParseIntPipe) idAerolinea: number,
-    @Body() updateAerolineaDto: UpdateAerolineaDto,
+    @Param('idAerolinea', ParseIntPipe)
+    idAerolinea: number,
+    @Body()
+    updateAerolineaDto: UpdateAerolineaDto,
   ) {
-    return this.aerolineasService.update(idAerolinea, updateAerolineaDto);
+    return this.aerolineasService.update(
+      idAerolinea,
+      updateAerolineaDto,
+    );
   }
 
   @Delete(':idAerolinea')
-  remove(@Param('idAerolinea', ParseIntPipe) idAerolinea: number) {
-    return this.aerolineasService.remove(idAerolinea);
+  remove(
+    @Param('idAerolinea', ParseIntPipe)
+    idAerolinea: number,
+  ) {
+    return this.aerolineasService.remove(
+      idAerolinea,
+    );
   }
 }
