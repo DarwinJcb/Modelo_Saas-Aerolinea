d6f484c3cc0eac760a2407ec67cf5b0c6bb5bc21
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-28 01:40:34 -0500

    Commit #71

--- saas-backend/src/app.module.ts
+++ saas-backend/src/app.module.ts
@@ -1,22 +1,20 @@
 /* saas-backend/src/app.module.ts */
 import { Module } from '@nestjs/common';
+import { AerolineasModule } from './aerolineas/aerolineas.module';
+import { AeropuertosModule } from './aeropuertos/aeropuertos.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { AvionesModule } from './aviones/aviones.module';
+import { BoletosModule } from './boletos/boletos.module';
+import { PasajerosModule } from './pasajeros/pasajeros.module';
 import { PlanesModule } from './planes/planes.module';
-import { AerolineasModule } from './aerolineas/aerolineas.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ReservasModule } from './reservas/reservas.module';
+import { RutasModule } from './rutas/rutas.module';
 import { SuscripcionesModule } from './suscripciones/suscripciones.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
-import { AvionesModule } from './aviones/aviones.module';
-import { AeropuertosModule } from './aeropuertos/aeropuertos.module';
-import { RutasModule } from './rutas/rutas.module';
 import { VuelosModule } from './vuelos/vuelos.module';
-import { PasajerosModule } from './pasajeros/pasajeros.module';
-import { ReservasModule } from './reservas/reservas.module';
-import { BoletosModule } from './boletos/boletos.module';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -34,7 +32,7 @@ import { AuthModule } from './auth/auth.module';
     ReservasModule,
     BoletosModule,
   ],
-  controllers: [AppController, AuthController],
-  providers: [AppService, AuthService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
--- saas-backend/src/auth/auth.module.ts
+++ saas-backend/src/auth/auth.module.ts
@@ -1,5 +1,30 @@
 /* saas-backend/src/auth/auth.module.ts */
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
-@Module({})
+const jwtSecret = process.env.JWT_SECRET;
+
+if (!jwtSecret) {
+  throw new Error(
+    'La variable JWT_SECRET no está configurada en el archivo .env',
+  );
+}
+
+@Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: jwtSecret,
+      signOptions: {
+        expiresIn: '2h',
+      },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
+})
 export class AuthModule { }
