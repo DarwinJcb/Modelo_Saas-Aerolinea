fa783cf1eaf31efc8ebbed148a69e6a8ceeda4ef
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-28 23:35:15 -0500

    Commit #97

--- saas-backend/src/aeropuertos/aeropuertos.controller.ts
+++ saas-backend/src/aeropuertos/aeropuertos.controller.ts
@@ -1,23 +1,43 @@
 /* saas-backend/src/aeropuertos/aeropuertos.controller.ts */
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards, } from '@nestjs/common';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { RolUsuario } from '../generated/prisma/enums';
 import { AeropuertosService } from './aeropuertos.service';
 import { CreateAeropuertoDto } from './dto/create-aeropuerto.dto';
 import { UpdateAeropuertoDto } from './dto/update-aeropuerto.dto';
 
 @Controller('aeropuertos')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class AeropuertosController {
-  constructor(private readonly aeropuertosService: AeropuertosService) { }
+  constructor(
+    private readonly aeropuertosService: AeropuertosService,
+  ) { }
 
+  @Roles(RolUsuario.SUPERADMIN)
   @Post()
-  create(@Body() createAeropuertoDto: CreateAeropuertoDto) {
+  create(
+    @Body() createAeropuertoDto: CreateAeropuertoDto,
+  ) {
     return this.aeropuertosService.create(createAeropuertoDto);
   }
 
+  @Roles(
+    RolUsuario.SUPERADMIN,
+    RolUsuario.ADMIN_AEROLINEA,
+    RolUsuario.EMPLEADO,
+  )
   @Get()
   findAll() {
     return this.aeropuertosService.findAll();
   }
 
+  @Roles(
+    RolUsuario.SUPERADMIN,
+    RolUsuario.ADMIN_AEROLINEA,
+    RolUsuario.EMPLEADO,
+  )
   @Get(':idAeropuerto')
   findOne(
     @Param('idAeropuerto', ParseIntPipe)
@@ -26,15 +46,20 @@ export class AeropuertosController {
     return this.aeropuertosService.findOne(idAeropuerto);
   }
 
+  @Roles(RolUsuario.SUPERADMIN)
   @Patch(':idAeropuerto')
   update(
     @Param('idAeropuerto', ParseIntPipe)
     idAeropuerto: number,
     @Body() updateAeropuertoDto: UpdateAeropuertoDto,
   ) {
-    return this.aeropuertosService.update(idAeropuerto, updateAeropuertoDto);
+    return this.aeropuertosService.update(
+      idAeropuerto,
+      updateAeropuertoDto,
+    );
   }
 
+  @Roles(RolUsuario.SUPERADMIN)
   @Delete(':idAeropuerto')
   remove(
     @Param('idAeropuerto', ParseIntPipe)
