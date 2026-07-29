37bc623f0fe8bb2bebc53c76e6271511f15ddf4b
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-29 11:23:07 -0500

    Commit #114

--- saas-backend/src/auth/auth.controller.ts
+++ saas-backend/src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 /* saas-backend/src/auth/auth.controller.ts */
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards, } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Patch, Post, UseGuards, } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsuarioActual } from './decorators/usuario-actual.decorator';
+import { CambiarContrasenaDto } from './dto/cambiar-contrasena.dto';
 import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import type { UsuarioAutenticado } from './interfaces/auth.interface';
@@ -25,5 +26,20 @@ export class AuthController {
         usuario: UsuarioAutenticado,
     ) {
         return usuario;
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @HttpCode(HttpStatus.OK)
+    @Patch('cambiar-contrasena')
+    cambiarContrasena(
+        @Body()
+        cambiarContrasenaDto: CambiarContrasenaDto,
+        @UsuarioActual()
+        usuarioActual: UsuarioAutenticado,
+    ) {
+        return this.authService.cambiarContrasena(
+            cambiarContrasenaDto,
+            usuarioActual,
+        );
     }
 }
